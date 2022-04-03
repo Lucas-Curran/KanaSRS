ecabbd4cadaa6141d7a77e16d63c9e8329795caf
Lucas-Curran <708637@sylvaniastudents.org>
2022-04-03 12:26:37 -0400

    Add license from WanaKanaJava repo

--- app/src/main/java/com/example/jwriter/KanaConverter.java
+++ app/src/main/java/com/example/jwriter/KanaConverter.java
@@ -5,7 +5,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Code from https://github.com/MasterKale/WanaKanaJava
+ * Copyright (c) 2013 Matthew Miller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * From: https://github.com/MasterKale/WanaKanaJava
  */
 public class KanaConverter {
 
