8840af4240a56d93ed3d7202d85d9b396acd6567
Lucas-Curran <74155090+Lucas-Curran@users.noreply.github.com>
2022-04-13 12:55:03 -0400

    Menu UI changes

--- app/src/main/java/com/example/jwriter/MenuActivity.kt
+++ app/src/main/java/com/example/jwriter/MenuActivity.kt
@@ -1,9 +1,14 @@
 package com.example.jwriter
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.appcompat.widget.Toolbar
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MenuActivity : AppCompatActivity() {
@@ -15,7 +20,11 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+
         setContentView(R.layout.activity_menu)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         beginButton = findViewById(R.id.beginButton)
         beginButton.setOnClickListener {
@@ -31,5 +40,22 @@ class MenuActivity : AppCompatActivity() {
         settingsButton.setOnClickListener {
             startActivity(Intent(this, SettingsActivity::class.java))
         }
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        if (menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.report -> println("report")
+            R.id.faq -> println("faq")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
