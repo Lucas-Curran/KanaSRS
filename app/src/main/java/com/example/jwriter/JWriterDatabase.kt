605193892f7a608341340fb1817e8633e2346076
Lucas-Curran <74155090+Lucas-Curran@users.noreply.github.com>
2022-04-14 00:35:39 -0400

    Add kana entity and prepopulate database with .db file

--- app/src/main/java/com/example/jwriter/JWriterDatabase.kt
+++ app/src/main/java/com/example/jwriter/JWriterDatabase.kt
@@ -8,10 +8,11 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 import java.util.concurrent.Executors
 
 
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class, Kana::class], version = 1)
 abstract class JWriterDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
+    abstract fun kanaDao(): KanaDao
 
     companion object {
 
@@ -25,22 +26,57 @@ abstract class JWriterDatabase : RoomDatabase() {
         }
 
         private fun buildDatabase(context: Context): JWriterDatabase {
-            return Room.databaseBuilder(context, JWriterDatabase::class.java, "jwriter.db")
-                .addCallback(object : RoomDatabase.Callback() {
-                    override fun onCreate(db: SupportSQLiteDatabase) {
-                        super.onCreate(db)
-                        //pre-populate data
-                        Executors.newSingleThreadExecutor().execute {
-                            instance?.let {
-                                val newUser = User(0)
-                                it.userDao().insertAll(newUser)
-                            }
-                        }
-                    }
-                })
+
+
+
+            return Room.databaseBuilder(context, JWriterDatabase::class.java, "jwriter.db").createFromAsset("jwriter.db")
                 .allowMainThreadQueries()
                 .fallbackToDestructiveMigration()
                 .build()
+
+            //Code below is for manual data entry
+
+//            return Room.databaseBuilder(context, JWriterDatabase::class.java, "jwriter.db")
+//                .addCallback(object : RoomDatabase.Callback() {
+//                    override fun onCreate(db: SupportSQLiteDatabase) {
+//                        super.onCreate(db)
+//                        //pre-populate data
+//                        Executors.newSingleThreadExecutor().execute {
+//                            instance?.let {
+//                                //Create new user, and add fresh kana data to database
+//
+//                                val newUser = User(0)
+//                                it.userDao().insertAll(newUser)
+//                                var id = 0
+//                                for (letter in ReviewActivity.hiraganaList) {
+//                                    val newKana = Kana(
+//                                        id = id,
+//                                        letter = letter,
+//                                        reviewTime = null,
+//                                        level = null,
+//                                        isHiragana = true,
+//                                        hasLearned = false)
+//                                    it.kanaDao().insertAll(newKana)
+//                                    id++
+//                                }
+//                                for (letter in ReviewActivity.katakanaList) {
+//                                    val newKana = Kana(
+//                                        id = id,
+//                                        letter = letter,
+//                                        reviewTime = null,
+//                                        level = null,
+//                                        isHiragana = false,
+//                                        hasLearned = false)
+//                                    it.kanaDao().insertAll(newKana)
+//                                    id++
+//                                }
+//                            }
+//                        }
+//                    }
+//                })
+//                .allowMainThreadQueries()
+//                .fallbackToDestructiveMigration()
+//                .build()
         }
 
         fun destroyInstance() {
