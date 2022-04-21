13a01e03bd80cffcbc880abac6f396c3e9794e4b
Lucas-Curran <74155090+Lucas-Curran@users.noreply.github.com>
2022-04-21 12:53:37 -0400

    Quick fixes

--- app/src/main/java/com/example/jwriter/LessonActivity.kt
+++ app/src/main/java/com/example/jwriter/LessonActivity.kt
@@ -1,26 +1,23 @@
 package com.example.jwriter
 
 import android.annotation.SuppressLint
-import android.app.usage.ConfigurationStats
-import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.view.animation.LayoutAnimationController
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.children
 import androidx.core.view.get
 
 class LessonActivity : AppCompatActivity() {
 
-    private lateinit var kanaStrokeWebView: WebView
-    private lateinit var nextItemButton: ImageButton
-    private lateinit var previousItemButton: ImageButton
-    private lateinit var viewAnimator: ViewAnimator
-    private lateinit var kanaTextView: TextView
     private lateinit var rootViewAnimator: ViewAnimator
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -30,40 +27,37 @@ class LessonActivity : AppCompatActivity() {
         setContentView(R.layout.activity_lesson)
 
         rootViewAnimator = findViewById(R.id.rootViewAnimator)
-        kanaStrokeWebView = findViewById(R.id.strokeWebView)
-        nextItemButton = findViewById(R.id.nextItemButton)
-        previousItemButton = findViewById(R.id.previousItemButton)
-        viewAnimator = findViewById(R.id.viewAnimator)
-        kanaTextView = findViewById(R.id.kanaTextView)
-        rootViewAnimator = findViewById(R.id.rootViewAnimator)
 
         val animationIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_right)
         val animationOut = AnimationUtils.loadAnimation(this, R.anim.slide_out_left)
         val prevAnimIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_left)
         val prevAnimOut = AnimationUtils.loadAnimation(this, R.anim.slide_out_right)
 
-        viewAnimator.inAnimation = animationIn
-        viewAnimator.outAnimation = animationOut
         rootViewAnimator.inAnimation = animationIn
         rootViewAnimator.outAnimation = animationOut
 
-        for (i in  1..5) {
+        for (i in 1..5) {
             val newView = layoutInflater.inflate(R.layout.lesson_item, null)
 
             newView.findViewById<TextView>(R.id.kanaTextView).text = i.toString()
 
             val tempViewAnimator = newView.findViewById<ViewAnimator>(R.id.viewAnimator)
+            val nextButton = newView.findViewById<ImageButton>(R.id.nextItemButton)
+            val previousButton =  newView.findViewById<ImageButton>(R.id.previousItemButton)
+
             tempViewAnimator.inAnimation = animationIn
             tempViewAnimator.outAnimation = animationOut
+
             val tempKanaWebStroke = newView.findViewById<WebView>(R.id.strokeWebView)
 
             tempKanaWebStroke.settings.javaScriptEnabled = true
             tempKanaWebStroke.webViewClient = WebViewClient()
             tempKanaWebStroke.loadUrl("https://upload.wikimedia.org/wikipedia/commons/d/d8/Hiragana_%E3%81%82_stroke_order_animation.gif")
 
-
-            newView.findViewById<ImageButton>(R.id.nextItemButton).setOnClickListener {
-                if (tempViewAnimator.currentView == tempKanaWebStroke) {
+            nextButton.setOnClickListener {
+                //nextButton.isEnabled = false
+                //previousButton.isEnabled = false
+                if (tempViewAnimator.currentView == tempViewAnimator.getChildAt(tempViewAnimator.childCount-1)) {
                     rootViewAnimator.inAnimation = animationIn
                     rootViewAnimator.outAnimation = animationOut
                     rootViewAnimator.showNext()
@@ -74,8 +68,10 @@ class LessonActivity : AppCompatActivity() {
                 }
             }
 
-            newView.findViewById<ImageButton>(R.id.previousItemButton).setOnClickListener {
-                if (tempViewAnimator.currentView == tempKanaWebStroke) {
+           previousButton.setOnClickListener {
+               //nextButton.isEnabled = false
+               //previousButton.isEnabled = false
+                if (tempViewAnimator.currentView == tempViewAnimator.getChildAt(tempViewAnimator.childCount)) {
                     tempViewAnimator.inAnimation = prevAnimIn
                     tempViewAnimator.outAnimation = prevAnimOut
                     tempViewAnimator.showPrevious()
@@ -85,7 +81,6 @@ class LessonActivity : AppCompatActivity() {
                     rootViewAnimator.showPrevious()
                 }
             }
-
             rootViewAnimator.addView(newView)
         }
     }
