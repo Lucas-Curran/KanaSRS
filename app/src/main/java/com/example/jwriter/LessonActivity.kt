056d184fcdbf369a07b373665a72e069b93d2187
Lucas-Curran <74155090+Lucas-Curran@users.noreply.github.com>
2022-04-23 13:41:07 -0400

    Update UI changes to separate thread and add loading spinner

--- app/src/main/java/com/example/jwriter/LessonActivity.kt
+++ app/src/main/java/com/example/jwriter/LessonActivity.kt
@@ -1,10 +1,9 @@
 package com.example.jwriter
 
 import android.annotation.SuppressLint
-import android.os.Build
+import android.content.Intent
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.SystemClock
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
@@ -13,16 +12,20 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.children
 import androidx.core.view.get
+import com.google.android.material.button.MaterialButton
+import kotlin.concurrent.thread
 
 class LessonActivity : AppCompatActivity() {
 
     private lateinit var rootViewAnimator: ViewAnimator
     var animationTime = 0L
     private lateinit var kanaList: List<Kana>
+    private lateinit var loadingBar: ProgressBar
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +34,7 @@ class LessonActivity : AppCompatActivity() {
         setContentView(R.layout.activity_lesson)
 
         rootViewAnimator = findViewById(R.id.rootViewAnimator)
+        loadingBar = findViewById(R.id.lessonProgressBar)
 
         val animationIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_right)
         val animationOut = AnimationUtils.loadAnimation(this, R.anim.slide_out_left)
@@ -45,59 +49,88 @@ class LessonActivity : AppCompatActivity() {
         kanaList = JWriterDatabase.getInstance(this).kanaDao().getUnlearnedKana()
         val kanaConverter = KanaConverter(false)
 
-        for (kana in kanaList.subList(0, 5)) {
-            val newView = layoutInflater.inflate(R.layout.lesson_item, null)
+        val subList = kanaList.subList(0, 5)
 
-            newView.findViewById<TextView>(R.id.kanaTextView).text = kana.letter
-            newView.findViewById<TextView>(R.id.englishTextView).text = kanaConverter._hiraganaToRomaji(kana.letter)
+        Handler(Looper.getMainLooper()).post {
 
-            val tempViewAnimator = newView.findViewById<ViewAnimator>(R.id.viewAnimator)
-            val nextButton = newView.findViewById<ImageButton>(R.id.nextItemButton)
-            val previousButton =  newView.findViewById<ImageButton>(R.id.previousItemButton)
+            for ((index, kana) in subList.withIndex()) {
+                val newView = layoutInflater.inflate(R.layout.lesson_item, null)
 
-            tempViewAnimator.inAnimation = animationIn
-            tempViewAnimator.outAnimation = animationOut
+                newView.findViewById<TextView>(R.id.kanaTextView).text = kana.letter
+                newView.findViewById<TextView>(R.id.englishTextView).text =
+                    kanaConverter._hiraganaToRomaji(kana.letter)
 
-            val tempKanaWebStroke = newView.findViewById<WebView>(R.id.strokeWebView)
+                val tempViewAnimator = newView.findViewById<ViewAnimator>(R.id.viewAnimator)
+                val nextButton = newView.findViewById<ImageButton>(R.id.nextItemButton)
+                val previousButton = newView.findViewById<ImageButton>(R.id.previousItemButton)
 
-            tempKanaWebStroke.settings.javaScriptEnabled = true
-            tempKanaWebStroke.webViewClient = WebViewClient()
-            tempKanaWebStroke.loadUrl("https://upload.wikimedia.org/wikipedia/commons/d/d8/Hiragana_%E3%81%82_stroke_order_animation.gif")
+                tempViewAnimator.inAnimation = animationIn
+                tempViewAnimator.outAnimation = animationOut
 
-            nextButton.setOnSingleClickListener {
-                if (tempViewAnimator.currentView == tempViewAnimator.getChildAt(tempViewAnimator.childCount-1)) {
-                    rootViewAnimator.inAnimation = animationIn
-                    rootViewAnimator.outAnimation = animationOut
-                    rootViewAnimator.showNext()
-                    rootViewAnimator.postOnAnimationDelayed({
-                        tempViewAnimator.inAnimation = null
-                        tempViewAnimator.outAnimation = null
-                        tempViewAnimator.displayedChild = tempViewAnimator[0].id
-                    }, rootViewAnimator.inAnimation.duration)
-                } else {
-                    tempViewAnimator.inAnimation = animationIn
-                    tempViewAnimator.outAnimation = animationOut
-                    tempViewAnimator.showNext()
+                val tempKanaWebStroke = newView.findViewById<WebView>(R.id.strokeWebView)
+                tempKanaWebStroke.settings.javaScriptEnabled = true
+                tempKanaWebStroke.webViewClient = WebViewClient()
+                tempKanaWebStroke.loadUrl("https://upload.wikimedia.org/wikipedia/commons/d/d8/Hiragana_%E3%81%82_stroke_order_animation.gif")
+
+                nextButton.setOnSingleClickListener {
+
+                    if (tempViewAnimator.currentView == tempViewAnimator.getChildAt(tempViewAnimator.childCount - 1)) {
+                        println(rootViewAnimator.displayedChild)
+                        rootViewAnimator.inAnimation = animationIn
+                        rootViewAnimator.outAnimation = animationOut
+                        if (rootViewAnimator.displayedChild == subList.lastIndex) {
+                            val view = layoutInflater.inflate(R.layout.lesson_completed_dialog, null)
+                            val builder = AlertDialog.Builder(this).setView(view).create()
+                            view.findViewById<MaterialButton>(R.id.restartLessonButton).setOnClickListener {
+                                rootViewAnimator.displayedChild = 0
+                                builder.dismiss()
+                            }
+                            view.findViewById<MaterialButton>(R.id.beginQuizButton).setOnClickListener {
+                                startActivity(Intent(this, ReviewActivity::class.java))
+                            }
+                            builder.show()
+                        } else {
+                            rootViewAnimator.showNext()
+                            rootViewAnimator.postOnAnimationDelayed({
+                                tempViewAnimator.inAnimation = null
+                                tempViewAnimator.outAnimation = null
+                                tempViewAnimator.displayedChild = tempViewAnimator[0].id
+                            }, rootViewAnimator.inAnimation.duration)
+                        }
+                    } else {
+                        tempViewAnimator.inAnimation = animationIn
+                        tempViewAnimator.outAnimation = animationOut
+                        tempViewAnimator.showNext()
+                    }
+                }
+
+                previousButton.setOnSingleClickListener {
+
+                    if (rootViewAnimator.displayedChild == 0 && tempViewAnimator.displayedChild == 0) {
+                        return@setOnSingleClickListener
+                    }
+
+                    if (tempViewAnimator.currentView == tempViewAnimator.getChildAt(tempViewAnimator.childCount - 1)) {
+                        tempViewAnimator.inAnimation = prevAnimIn
+                        tempViewAnimator.outAnimation = prevAnimOut
+                        tempViewAnimator.showPrevious()
+                    } else {
+                        rootViewAnimator.inAnimation = prevAnimIn
+                        rootViewAnimator.outAnimation = prevAnimOut
+                        rootViewAnimator.showPrevious()
+                        rootViewAnimator.postOnAnimationDelayed({
+                            tempViewAnimator.inAnimation = null
+                            tempViewAnimator.outAnimation = null
+                            tempViewAnimator.displayedChild = 0
+                        }, rootViewAnimator.outAnimation.duration)
+                    }
+                }
+
+                rootViewAnimator.addView(newView)
+                if (index == subList.lastIndex) {
+                    rootViewAnimator.removeView(loadingBar)
                 }
             }
-
-           previousButton.setOnSingleClickListener {
-                if (tempViewAnimator.currentView == tempViewAnimator.getChildAt(tempViewAnimator.childCount-1)) {
-                    tempViewAnimator.inAnimation = prevAnimIn
-                    tempViewAnimator.outAnimation = prevAnimOut
-                    tempViewAnimator.showPrevious()
-                } else {
-                    rootViewAnimator.inAnimation = prevAnimIn
-                    rootViewAnimator.outAnimation = prevAnimOut
-                    rootViewAnimator.showPrevious()
-                    rootViewAnimator.postOnAnimationDelayed({
-                        tempViewAnimator.inAnimation = null
-                        tempViewAnimator.outAnimation = null
-                        tempViewAnimator.displayedChild = tempViewAnimator[0].id
-                    }, rootViewAnimator.outAnimation.duration)
-                }
-            }
-            rootViewAnimator.addView(newView)
         }
     }
 
