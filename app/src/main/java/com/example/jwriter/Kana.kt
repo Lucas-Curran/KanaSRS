d99e3db516d69120dde4f8f5db97c6f2eda0b513
Lucas Curran <708637@sylvaniastudents.org>
2022-04-25 14:06:36 -0400

    Add parcelize plugin and parcelize kana class

--- app/src/main/java/com/example/jwriter/Kana.kt
+++ app/src/main/java/com/example/jwriter/Kana.kt
@@ -1,10 +1,13 @@
 package com.example.jwriter
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity
+@Parcelize
 data class Kana(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "letter") val letter: String?,
@@ -14,4 +17,4 @@ data class Kana(
     @ColumnInfo(name = "hasLearned") var hasLearned: Boolean,
     @ColumnInfo(name = "gif") val gif: String,
     @ColumnInfo(name = "description") val description: String
-)
+) : Parcelable
