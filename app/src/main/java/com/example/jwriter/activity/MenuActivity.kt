4ac3306103452d032fb45b8491ce184eb144673d
Lucas-Curran <708637@sylvaniastudents.org>
2022-04-30 20:45:04 -0400

    Add notifications for number of reviews

--- app/src/main/java/com/example/jwriter/activity/MenuActivity.kt
+++ app/src/main/java/com/example/jwriter/activity/MenuActivity.kt
@@ -1,15 +1,14 @@
 package com.example.jwriter.activity
 
 import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.menu.MenuBuilder
 import androidx.appcompat.widget.Toolbar
@@ -17,8 +16,11 @@ import androidx.core.graphics.BlendModeColorFilterCompat
 import androidx.core.graphics.BlendModeCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.jwriter.*
+import com.example.jwriter.notification.NotificationReceiver
+import me.leolin.shortcutbadger.ShortcutBadger
 import java.time.Duration
 import java.util.*
+
 
 class MenuActivity : AppCompatActivity() {
 
@@ -27,6 +29,8 @@ class MenuActivity : AppCompatActivity() {
     private lateinit var settingsButton: Button
     private lateinit var lessonButton: Button
     private lateinit var showMoreLayout: RelativeLayout
+    private lateinit var summaryLayout: RelativeLayout
+    private lateinit var levelsLayout: LinearLayout
     private lateinit var showMoreArrow: ImageView
     private lateinit var summaryButton: Button
     private var showMore = true
@@ -34,7 +38,6 @@ class MenuActivity : AppCompatActivity() {
     private var numItemsToReview: Int = 0
     private var mostRecentReview = Long.MAX_VALUE
     private var kanaToReview = ArrayList<Kana>()
-    private var levelNames = arrayOf("Rookie", "Amateur", "Expert", "Master", "Sensei")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,6 +61,8 @@ class MenuActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_menu)
 
+        myAlarm()
+
         val numReviewTextView = findViewById<TextView>(R.id.numItemsTextView)
         numReviewTextView.text = numItemsToReview.toString()
 
@@ -73,16 +78,22 @@ class MenuActivity : AppCompatActivity() {
             R.color.white, BlendModeCompat.SRC_ATOP)
         setSupportActionBar(toolbar)
 
-        summaryButton = findViewById(R.id.summary)
+        summaryButton = findViewById(R.id.summaryButton)
         showMoreLayout = findViewById(R.id.showMoreRelativeLayout)
         showMoreArrow = findViewById(R.id.arrowImageView)
+        summaryLayout = findViewById(R.id.summaryRelativeLayout)
         showMoreArrow.setOnClickListener {
             if (showMore && !moving) {
                 moving = true
                 AnimUtilities.slideView(
+                    summaryLayout,
+                    summaryLayout.height,
+                    summaryLayout.height + 800
+                ) {}
+                AnimUtilities.slideView(
                     summaryButton,
                     summaryButton.height,
-                    summaryButton.height + 500
+                    summaryButton.height + 800
                 ) {
                     showMore = false
                     moving = false
@@ -91,15 +102,37 @@ class MenuActivity : AppCompatActivity() {
             } else if (!showMore && !moving) {
                 moving = true
                 AnimUtilities.slideView(
+                    summaryLayout,
+                    summaryLayout.height,
+                    summaryLayout.height - 800
+                ) {}
+                AnimUtilities.slideView(
                     summaryButton,
                     summaryButton.height,
-                    summaryButton.height - 500
+                    summaryButton.height - 800
                 ) {
                     moving = false
                     showMore = true
                 }
                 showMoreArrow.setImageResource(android.R.drawable.arrow_down_float)
             }
+        }
+
+        levelsLayout = findViewById(R.id.levelsLayout)
+        levelsLayout.findViewById<LinearLayout>(R.id.rookieLinearLayout).setOnClickListener {
+            println("rookie")
+        }
+        levelsLayout.findViewById<LinearLayout>(R.id.amateurLinearLayout).setOnClickListener {
+            println("amateur")
+        }
+        levelsLayout.findViewById<LinearLayout>(R.id.expertLinearLayout).setOnClickListener {
+            println("expert")
+        }
+        levelsLayout.findViewById<LinearLayout>(R.id.masterLinearLayout).setOnClickListener {
+            println("master")
+        }
+        levelsLayout.findViewById<LinearLayout>(R.id.senseiLinearLayout).setOnClickListener {
+            println("sensei")
         }
 
         lessonButton = findViewById(R.id.lessonButton)
@@ -127,8 +160,6 @@ class MenuActivity : AppCompatActivity() {
             startActivity(Intent(this, SettingsActivity::class.java))
         }
     }
-
-
 
     @SuppressLint("RestrictedApi")
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -165,6 +196,29 @@ class MenuActivity : AppCompatActivity() {
         } else {
             return ""
         }
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    fun myAlarm() {
+        val calendar = Calendar.getInstance()
+        calendar[Calendar.HOUR_OF_DAY] = 20
+        calendar[Calendar.MINUTE] = 43
+        calendar[Calendar.SECOND] = 0
+        if (calendar.time < Date()) calendar.add(Calendar.DAY_OF_MONTH, 1)
+        val intent = Intent(applicationContext, NotificationReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,
+            pendingIntent
+        )
     }
 
 }
