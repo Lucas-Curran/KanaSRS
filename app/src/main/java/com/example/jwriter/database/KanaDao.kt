416919f706a3abf1e5d1804904af667d144a599d
Lucas-Curran <708637@sylvaniastudents.org>
2022-05-10 12:35:25 -0400

    Add get hiragana/katakana queries

--- app/src/main/java/com/example/jwriter/database/KanaDao.kt
+++ app/src/main/java/com/example/jwriter/database/KanaDao.kt
@@ -12,6 +12,12 @@ interface KanaDao {
     @Query("SELECT * FROM kana WHERE hasLearned = 0")
     fun getUnlearnedKana(): List<Kana>
 
+    @Query("SELECT * FROM kana WHERE isHiragana = 1")
+    fun getHiragana(): List<Kana>
+
+    @Query("SELECT * FROM kana WHERE isHiragana = 0")
+    fun getKatakana(): List<Kana>
+
     @Insert
     fun insertAll(vararg kana: Kana)
 
