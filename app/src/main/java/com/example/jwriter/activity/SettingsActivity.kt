d367fcc1de6fce8548d659703cff82a21e2cbe00
Lucas-Curran <708637@sylvaniastudents.org>
2022-05-20 01:33:14 -0400

    Add reset account function

--- app/src/main/java/com/example/jwriter/activity/SettingsActivity.kt
+++ app/src/main/java/com/example/jwriter/activity/SettingsActivity.kt
@@ -1,8 +1,16 @@
 package com.example.jwriter.activity
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.Gravity
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.jwriter.R
+import com.example.jwriter.database.JWriterDatabase
 
 /*
 Could possibly include:
@@ -14,8 +22,35 @@ Could possibly include:
  */
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var resetAccount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_settings)
+
+        resetAccount = findViewById(R.id.resetAccountTextView)
+        resetAccount.setOnClickListener {
+
+            val view = layoutInflater.inflate(R.layout.reset_account_dialog, null)
+            val dialog = AlertDialog.Builder(this, R.style.DialogTheme).setView(view).create()
+
+            view.findViewById<TextView>(R.id.cancelButton).setOnClickListener {
+                Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
+            }
+            view.findViewById<TextView>(R.id.resetAccountTextView).setOnClickListener {
+                this.deleteDatabase("jwriter.db")
+                JWriterDatabase.destroyInstance()
+                val toast = Toast(this)
+                toast.setGravity(Gravity.CENTER, 0, 0)
+                toast.setText(resources.getText(R.string.clear_account))
+                toast.show()
+                startActivity(Intent(this, MenuActivity::class.java))
+            }
+
+            dialog.show()
+        }
     }
 }
