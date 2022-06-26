b679f8e431080ee3016321b5b601cdd17724becb
Lucas-Curran <708637@sylvaniastudents.org>
2022-06-26 14:24:18 -0400

    Add more query functions

--- app/src/main/java/com/email/contact/kanasrs/database/KanaDao.kt
+++ app/src/main/java/com/email/contact/kanasrs/database/KanaDao.kt
@@ -11,6 +11,12 @@ interface KanaDao {
     @Query("SELECT * FROM kana WHERE hasLearned = 0")
     fun getUnlearnedKana(): List<Kana>
 
+    @Query("SELECT SUM(totalCorrect) FROM kana")
+    fun sumCorrect(): Int
+
+    @Query("SELECT SUM(totalAnswered) FROM kana")
+    fun sumAnswered(): Int
+
     @Query("SELECT * FROM kana WHERE hasLearned = 1 and isHiragana = 1")
     fun getLearnedHiragana(): List<Kana>
 
@@ -22,6 +28,21 @@ interface KanaDao {
 
     @Query("SELECT * FROM kana WHERE isHiragana = 0")
     fun getKatakana(): List<Kana>
+
+    @Query("SELECT * FROM kana WHERE level = 1 or level = 2")
+    fun getRookieKana(): List<Kana>
+
+    @Query("SELECT * FROM kana WHERE level = 3")
+    fun getAmateurKana(): List<Kana>
+
+    @Query("SELECT * FROM kana WHERE level = 4")
+    fun getExpertKana(): List<Kana>
+
+    @Query("SELECT * FROM kana WHERE level = 5")
+    fun getMasterKana(): List<Kana>
+
+    @Query("SELECT * FROM kana WHERE level = 6")
+    fun getSenseiKana(): List<Kana>
 
     @Insert
     fun insertAll(vararg kana: Kana)
