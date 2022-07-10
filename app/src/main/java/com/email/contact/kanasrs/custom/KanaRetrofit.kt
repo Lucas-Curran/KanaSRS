a8e12c8ad62f31a7222a8f0317857738f0e0f337
Lucas-Curran <708637@sylvaniastudents.org>
2022-07-10 03:41:37 -0400

    Retrofit interface for checking handwriting

---
+++ app/src/main/java/com/email/contact/kanasrs/custom/KanaRetrofit.kt
@@ -0,0 +1,11 @@
+package com.email.contact.kanasrs.custom
+
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.*
+
+interface KanaRetrofit {
+    @POST("predict/")
+    fun writingToKana(@Body data: RequestBody): Call<ResponseBody>
+}
