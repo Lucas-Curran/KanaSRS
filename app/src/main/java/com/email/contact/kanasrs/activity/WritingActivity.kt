dd2005d2b6f7e77088a0d783eeeeccae363707dc
Lucas-Curran <708637@sylvaniastudents.org>
2022-07-11 15:52:41 -0400

    Check internet method + correctly use incorrect/correct animations

--- app/src/main/java/com/email/contact/kanasrs/activity/WritingActivity.kt
+++ app/src/main/java/com/email/contact/kanasrs/activity/WritingActivity.kt
@@ -1,26 +1,27 @@
 package com.email.contact.kanasrs.activity
 
 import android.animation.Animator
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
-import android.view.animation.Animation
+import android.view.View
 import android.view.animation.AnimationUtils
-import android.widget.Button
-import android.widget.RelativeLayout
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
-import com.airbnb.lottie.animation.keyframe.BaseKeyframeAnimation
 import com.email.contact.kanasrs.R
 import com.email.contact.kanasrs.custom.DrawingView
 import com.email.contact.kanasrs.database.Kana
 import com.email.contact.kanasrs.util.KanaConverter
+import com.email.contact.kanasrs.util.Utilities.Companion.disable
+import com.email.contact.kanasrs.util.Utilities.Companion.enable
 import com.google.android.material.button.MaterialButton
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+
 
 class WritingActivity : AppCompatActivity() {
 
@@ -31,7 +32,10 @@ class WritingActivity : AppCompatActivity() {
     private lateinit var wrongImageOne: LottieAnimationView
     private lateinit var wrongImageTwo: LottieAnimationView
     private lateinit var wrongImageThree: LottieAnimationView
+    private lateinit var correctAnimation: LottieAnimationView
     private lateinit var wrongImages: List<LottieAnimationView>
+    private lateinit var submitWriting: Button
+    private lateinit var loadResultBar: ProgressBar
     private val imagesAnimatedList = mutableListOf(false, false, false)
     private lateinit var kanaConverter: KanaConverter
     private var wrongCounter = 0
@@ -43,6 +47,8 @@ class WritingActivity : AppCompatActivity() {
 
         letterToDraw = findViewById(R.id.letterToDraw)
         kanaConverter = KanaConverter(false)
+        correctAnimation = findViewById(R.id.correctAnimation)
+        loadResultBar = findViewById(R.id.loadingResultBar)
 
         val relativeLayout = findViewById<RelativeLayout>(R.id.writingRelativeLayout)
         drawingView = DrawingView(this)
@@ -58,7 +64,7 @@ class WritingActivity : AppCompatActivity() {
         wrongImageTwo = findViewById(R.id.secondWrong)
         wrongImageThree = findViewById(R.id.thirdWrong)
         wrongImages = listOf(wrongImageOne, wrongImageTwo, wrongImageThree)
-        wrongImages.forEachIndexed { index, lottieAnimationView ->
+        wrongImages.forEach { lottieAnimationView ->
             lottieAnimationView.addAnimatorListener(object : Animator.AnimatorListener {
                 override fun onAnimationStart(p0: Animator?) {
                 }
@@ -67,6 +73,9 @@ class WritingActivity : AppCompatActivity() {
                     // If animation is in reverse, i.e. disappearing, then switch it back
                     if (lottieAnimationView.speed < 0) {
                         lottieAnimationView.reverseAnimationSpeed()
+                    }
+                    if (imagesAnimatedList[2]) {
+                        nextKana()
                     }
                 }
 
@@ -78,20 +87,51 @@ class WritingActivity : AppCompatActivity() {
             })
         }
 
-        val submitWriting = findViewById<MaterialButton>(R.id.submitWritingButton)
+        correctAnimation.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator?) {
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                correctAnimation.reverseAnimationSpeed()
+                if (correctAnimation.speed < 0) {
+                    correctAnimation.playAnimation()
+                }
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+            }
+
+            override fun onAnimationRepeat(p0: Animator?) {
+            }
+
+        })
+
+        submitWriting = findViewById<MaterialButton>(R.id.submitWritingButton)
         val shakeAnimation = AnimationUtils.loadAnimation(this, R.anim.shake)
 
         submitWriting.setOnClickListener {
+
+            if (!isInternetAvailable(this)) {
+                Toast.makeText(this, "Error: please check your internet connection", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             if (drawingView.checkIfEmpty()) {
                 Toast.makeText(this, "Please write something", Toast.LENGTH_SHORT).show()
             } else {
+                loadResultBar.visibility = View.VISIBLE
+                submitWriting.disable()
                 GlobalScope.launch {
-                    if (drawingView.isDrawingCorrect(testList[0])) {
-                        nextKana()
+                    if (drawingView.isDrawingCorrect(testList[0], loadResultBar)) {
+                        runOnUiThread {
+                            correctAnimation.playAnimation()
+                            nextKana()
+                        }
                     } else {
                         wrongCounter++
                         drawingView.clearDrawing()
                         runOnUiThread {
+                            submitWriting.enable()
                             submitWriting.startAnimation(shakeAnimation)
                             when (wrongCounter) {
                                 1 -> {
@@ -105,7 +145,6 @@ class WritingActivity : AppCompatActivity() {
                                 3 -> {
                                     wrongImageThree.playAnimation()
                                     imagesAnimatedList[2] = true
-                                    nextKana()
                                 }
                             }
                         }
@@ -120,19 +159,52 @@ class WritingActivity : AppCompatActivity() {
 
     }
 
+    private fun isInternetAvailable(context: Context): Boolean {
+        var result = false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
+
+                }
+            }
+        }
+        return result
+    }
+
     private fun nextKana() {
         wrongCounter = 0
         testList.removeAt(0)
         drawingView.clearDrawing()
-        runOnUiThread {
+        letterToDraw.animate().alpha(0f).withEndAction {
             letterToDraw.text = kanaConverter.hiraganaToRomaji(testList[0])
-            //Reverse each animation, play it, and reverse it back, making them all disappear
-            wrongImages.forEachIndexed { index, lottieAnimationView ->
-                if (imagesAnimatedList[index]) {
-                    lottieAnimationView.reverseAnimationSpeed()
-                    lottieAnimationView.playAnimation()
-                    imagesAnimatedList[index] = false
-                }
+            letterToDraw.animate().alpha(1f).setStartDelay(750).withEndAction {
+                submitWriting.enable()
+            }.duration = 500
+        }.duration = 500
+        //Reverse each animation, play it, making them all disappear, only if they've already been animated
+        wrongImages.forEachIndexed { index, lottieAnimationView ->
+            if (imagesAnimatedList[index]) {
+                imagesAnimatedList[index] = false
+                lottieAnimationView.reverseAnimationSpeed()
+                lottieAnimationView.playAnimation()
             }
         }
     }
