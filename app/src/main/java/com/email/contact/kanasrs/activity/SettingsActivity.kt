534a0d28442947a7abb52f0c123d5317e6964f60
Lucas-Curran <708637@sylvaniastudents.org>
2022-07-18 00:12:28 -0400

    Add randomize font setting option

--- app/src/main/java/com/email/contact/kanasrs/activity/SettingsActivity.kt
+++ app/src/main/java/com/email/contact/kanasrs/activity/SettingsActivity.kt
@@ -40,7 +40,9 @@ class SettingsActivity : AppCompatActivity() {
     private lateinit var sentAtTextView: TextView
     private lateinit var lessonNumberRadioGroup: RadioGroup
     private lateinit var writingButton: Button
-    private lateinit var extraInfoWriting: LottieAnimationView
+    private lateinit var randomizeButton: Button
+    private lateinit var extraInfoRandomize: ImageView
+    private lateinit var extraInfoWriting: ImageView
 
     private var sleepingCatCounter = 0
     private var catToast: Toast? = null
@@ -69,7 +71,10 @@ class SettingsActivity : AppCompatActivity() {
         sentAtTextView = findViewById(R.id.sentAtTextView)
         lessonNumberRadioGroup = findViewById(R.id.lessonNumberRadioGroup)
         writingButton = findViewById(R.id.writingButton)
+        randomizeButton = findViewById(R.id.randomizeButton)
+        extraInfoRandomize = findViewById(R.id.randomizeInfoImage)
         extraInfoWriting = findViewById(R.id.writingInfoImage)
+
 
          val gradientDrawable = GradientDrawable(
             GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(
@@ -107,52 +112,58 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         extraInfoWriting.setOnClickListener {
-            val view = layoutInflater.inflate(R.layout.extra_info_dialog, null)
-            val dialog = AlertDialog.Builder(this, R.style.DialogTheme).setView(view).create()
+            showInfoDialog(R.string.writing_info, "Writing Info")
+        }
 
-            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-
-            val infoText = view.findViewById<TextView>(R.id.infoTextView)
-
-            infoText.text = HtmlCompat.fromHtml(getString(R.string.writing_info), HtmlCompat.FROM_HTML_MODE_LEGACY)
-            view.findViewById<TextView>(R.id.titleTextView).text = "Writing Info"
-            view.findViewById<LottieAnimationView>(R.id.closeAnimation).setOnClickListener {
-                dialog.dismiss()
-            }
-
-            infoText.movementMethod = ScrollingMovementMethod()
-            infoText.setOnTouchListener { v, event ->
-                v.parent.requestDisallowInterceptTouchEvent(true)
-                when (event.action and MotionEvent.ACTION_MASK) {
-                    MotionEvent.ACTION_UP ->
-                        v.parent.requestDisallowInterceptTouchEvent(false)
-                }
-                false
-            }
-
-            dialog.show()
+        extraInfoRandomize.setOnClickListener {
+            showInfoDialog(R.string.random_font_info, "Random Font Info")
         }
 
         val sharedPref = getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE)
 
         if (sharedPref.getBoolean("kanasrsWritingEnabled", true))  {
-            writingButton.text = "Writing enabled"
+            writingButton.text = "Writing\nenabled"
             writingButton.isSelected = true
         } else {
-            writingButton.text = "Writing disabled"
+            writingButton.text = "Writing\ndisabled"
             writingButton.isSelected = false
+        }
+
+        if (sharedPref.getBoolean("randomizedFonts", false)) {
+            randomizeButton.text = "Randomize fonts\nenabled"
+            randomizeButton.isSelected = true
+        } else {
+            randomizeButton.text = "Randomize fonts\ndisabled"
+            randomizeButton.isSelected = false
+        }
+
+        randomizeButton.setOnClickListener {
+            randomizeButton.isSelected = !randomizeButton.isSelected
+            if (randomizeButton.isSelected) {
+                randomizeButton.text = "Randomize fonts\nenabled"
+                with (sharedPref.edit()) {
+                    putBoolean("randomizedFonts", true)
+                    apply()
+                }
+            } else {
+                randomizeButton.text = "Randomize fonts\ndisabled"
+                with (sharedPref.edit()) {
+                    putBoolean("randomizedFonts", false)
+                    apply()
+                }
+            }
         }
 
         writingButton.setOnClickListener {
             writingButton.isSelected = !writingButton.isSelected
             if (writingButton.isSelected) {
-                writingButton.text = "Writing enabled"
+                writingButton.text = "Writing\nenabled"
                 with (sharedPref.edit()) {
                     putBoolean("kanasrsWritingEnabled", true)
                     apply()
                 }
             } else {
-                writingButton.text = "Writing disabled"
+                writingButton.text = "Writing\ndisabled"
                 with (sharedPref.edit()) {
                     putBoolean("kanasrsWritingEnabled", false)
                     apply()
@@ -236,6 +247,34 @@ class SettingsActivity : AppCompatActivity() {
         val sdfs = SimpleDateFormat("hh:mm a")
         val date = sdf.parse("$hour:$minute")
         return sdfs.format(date)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun showInfoDialog(stringResource: Int, title: String) {
+        val view = layoutInflater.inflate(R.layout.extra_info_dialog, null)
+        val dialog = AlertDialog.Builder(this, R.style.DialogTheme).setView(view).create()
+
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val infoText = view.findViewById<TextView>(R.id.infoTextView)
+
+        infoText.text = HtmlCompat.fromHtml(getString(stringResource), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        view.findViewById<TextView>(R.id.titleTextView).text = title
+        view.findViewById<LottieAnimationView>(R.id.closeAnimation).setOnClickListener {
+            dialog.dismiss()
+        }
+
+        infoText.movementMethod = ScrollingMovementMethod()
+        infoText.setOnTouchListener { v, event ->
+            v.parent.requestDisallowInterceptTouchEvent(true)
+            when (event.action and MotionEvent.ACTION_MASK) {
+                MotionEvent.ACTION_UP ->
+                    v.parent.requestDisallowInterceptTouchEvent(false)
+            }
+            false
+        }
+
+        dialog.show()
     }
 
 }
