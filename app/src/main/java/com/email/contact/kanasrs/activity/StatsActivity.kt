9213b947b4f56c48570dd266019fcfca33ac6585
Lucas-Curran <708637@sylvaniastudents.org>
2022-07-28 00:17:38 -0400

    Add writing info for everything, accuracy etc..

--- app/src/main/java/com/email/contact/kanasrs/activity/StatsActivity.kt
+++ app/src/main/java/com/email/contact/kanasrs/activity/StatsActivity.kt
@@ -4,28 +4,38 @@ import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.content.res.ColorStateList
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.animation.doOnEnd
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.setMargins
 import com.airbnb.lottie.LottieAnimationView
-import com.email.contact.kanasrs.custom.KanaInfoView
 import com.email.contact.kanasrs.R
-import com.email.contact.kanasrs.database.KanaSRSDatabase
+import com.email.contact.kanasrs.custom.DistributionBar
+import com.email.contact.kanasrs.custom.KanaInfoView
 import com.email.contact.kanasrs.database.Kana
+import com.email.contact.kanasrs.database.KanaSRSDatabase
 import com.email.contact.kanasrs.util.Utilities
 import com.email.contact.kanasrs.util.Utilities.Companion.ROOKIE1
 import com.email.contact.kanasrs.util.Utilities.Companion.ROOKIE2
+import com.email.contact.kanasrs.util.Utilities.Companion.dpToPx
 import com.email.contact.kanasrs.util.Utilities.Companion.getLevelColor
+import com.email.contact.kanasrs.util.Utilities.Companion.pxToDp
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.GRAVITY_CENTER
+import com.google.android.material.tabs.TabLayout.MODE_FIXED
 import com.skydoves.progressview.ProgressLabelConstraints
 import com.skydoves.progressview.ProgressView
 import com.skydoves.progressview.progressView
@@ -45,8 +55,16 @@ class StatsActivity : AppCompatActivity() {
     private lateinit var tabLayout: TabLayout
     private lateinit var hiraganaProgressViews: ArrayList<ProgressView>
     private lateinit var katakanaProgressViews: ArrayList<ProgressView>
+    private lateinit var hiraganaWritingProgressViews: ArrayList<ProgressView>
+    private lateinit var katakanaWritingProgressViews: ArrayList<ProgressView>
     private lateinit var progressScrollView: ScrollView
     private lateinit var overallView: View
+
+    private lateinit var accuracyProgressBar: ProgressView
+    private lateinit var totalCorrectText: TextView
+    private lateinit var totalAnsweredText: TextView
+
+    private lateinit var kanaDistributionBar: DistributionBar
 
     private var mostRecentReview = Long.MAX_VALUE
 
@@ -66,6 +84,8 @@ class StatsActivity : AppCompatActivity() {
 
     private var fading = false
 
+    private var isOnHiragana = true
+
     object TabConstants {
         const val OVERALL = 0
         const val HIRAGANA = 1
@@ -78,6 +98,8 @@ class StatsActivity : AppCompatActivity() {
 
         hiraganaProgressViews = ArrayList()
         katakanaProgressViews = ArrayList()
+        hiraganaWritingProgressViews = ArrayList()
+        katakanaWritingProgressViews = ArrayList()
 
         overallView = layoutInflater.inflate(R.layout.stats_overall_tab, null)
 
@@ -131,13 +153,13 @@ class StatsActivity : AppCompatActivity() {
 
         hiraganaLayout.setOnLongClickListener {
             if (learnedHiragana.isNotEmpty()) {
-                KanaInfoView(this, learnedHiragana[currentHiragana], true).show()
+                KanaInfoView(this, learnedHiragana[currentHiragana], true, writingInfo = false).show()
             }
             false
         }
         katakanaLayout.setOnLongClickListener {
             if (learnedKatakana.isNotEmpty()) {
-                KanaInfoView(this, learnedKatakana[currentKatakana], true).show()
+                KanaInfoView(this, learnedKatakana[currentKatakana], true, writingInfo = false).show()
             }
             false
         }
@@ -231,15 +253,56 @@ class StatsActivity : AppCompatActivity() {
             }
         }
 
+        accuracyProgressBar = overallView.findViewById(R.id.totalAccuracyProgress)
+        totalCorrectText = overallView.findViewById(R.id.totalCorrectText)
+        totalAnsweredText = overallView.findViewById(R.id.totalText)
+        kanaDistributionBar = overallView.findViewById(R.id.distributionBar)
 
-        val accuracyProgressBar = overallView.findViewById<ProgressView>(R.id.totalAccuracyProgress)
-        accuracyProgressBar.max = KanaSRSDatabase.getInstance(this).kanaDao().sumAnswered().toFloat()
-        accuracyProgressBar.progress = KanaSRSDatabase.getInstance(this).kanaDao().sumCorrect().toFloat()
-        overallView.findViewById<TextView>(R.id.totalCorrectText).text = KanaSRSDatabase.getInstance(this).kanaDao().sumCorrect().toString()
-        overallView.findViewById<TextView>(R.id.totalText).text = KanaSRSDatabase.getInstance(this).kanaDao().sumAnswered().toString()
+        recallTab()
+
+        val modeTabs = overallView.findViewById<TabLayout>(R.id.modeTabs)
+
+        for (i in 0 until modeTabs.tabCount) {
+            val tab = (modeTabs.getChildAt(0) as ViewGroup).getChildAt(i)
+            val p = tab.layoutParams as ViewGroup.MarginLayoutParams
+            p.setMargins(15, 0, 15, 0)
+            tab.requestLayout()
+        }
+
+        modeTabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> recallTab()
+                    1 -> writingTab()
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
 
         loadUserStats()
         overallTab()
+    }
+
+    private fun recallTab() {
+        accuracyProgressBar.max = KanaSRSDatabase.getInstance(this).kanaDao().sumAnswered().toFloat()
+        accuracyProgressBar.progress = KanaSRSDatabase.getInstance(this).kanaDao().sumCorrect().toFloat()
+        totalCorrectText.text = KanaSRSDatabase.getInstance(this).kanaDao().sumCorrect().toString()
+        totalAnsweredText.text = KanaSRSDatabase.getInstance(this).kanaDao().sumAnswered().toString()
+        kanaDistributionBar.setToRecall()
+    }
+
+    private fun writingTab() {
+        accuracyProgressBar.max = KanaSRSDatabase.getInstance(this).kanaDao().sumWritingAnswered().toFloat()
+        accuracyProgressBar.progress = KanaSRSDatabase.getInstance(this).kanaDao().sumWritingCorrect().toFloat()
+        totalCorrectText.text = KanaSRSDatabase.getInstance(this).kanaDao().sumWritingCorrect().toString()
+        totalAnsweredText.text = KanaSRSDatabase.getInstance(this).kanaDao().sumWritingAnswered().toString()
+        kanaDistributionBar.setToWriting()
     }
 
     /**
@@ -247,8 +310,10 @@ class StatsActivity : AppCompatActivity() {
      */
     private fun loadUserStats() {
 
-        loadBars(KanaSRSDatabase.getInstance(this).kanaDao().getHiragana())
-        loadBars(KanaSRSDatabase.getInstance(this).kanaDao().getKatakana())
+        loadBars(KanaSRSDatabase.getInstance(this).kanaDao().getHiragana(), false)
+        loadBars(KanaSRSDatabase.getInstance(this).kanaDao().getKatakana(), false)
+        loadBars(KanaSRSDatabase.getInstance(this).kanaDao().getHiragana(), true)
+        loadBars(KanaSRSDatabase.getInstance(this).kanaDao().getKatakana(), true)
 
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
 
@@ -295,8 +360,12 @@ class StatsActivity : AppCompatActivity() {
      * Hiragana progress tab
      */
     private fun hiraganaTab() {
+        isOnHiragana = true
         //Remove current layout, loop through hiragana progress bars and add them to the scroll view
         progressLinearLayout.removeAllViewsInLayout()
+
+        addTabs()
+
         for (view in hiraganaProgressViews) {
             progressLinearLayout.addView(view)
         }
@@ -319,8 +388,12 @@ class StatsActivity : AppCompatActivity() {
      * Katakana progress tab
      */
     private fun katakanaTab() {
+        isOnHiragana = false
         //Remove current layout, loop through katakana progress bars and add them to the scroll view
         progressLinearLayout.removeAllViewsInLayout()
+
+        addTabs()
+
         for (view in katakanaProgressViews) {
             progressLinearLayout.addView(view)
         }
@@ -338,19 +411,140 @@ class StatsActivity : AppCompatActivity() {
         constraintSet.applyTo(constraintLayout)
     }
 
+    private fun addTabs() {
+        val typeTabLayout = TabLayout(this)
+        val recallTab = typeTabLayout.newTab()
+        recallTab.icon = ContextCompat.getDrawable(this, R.drawable.ic_brain)
+        val writingTab = typeTabLayout.newTab()
+        writingTab.icon = ContextCompat.getDrawable(this, R.drawable.ic_pencil)
+        typeTabLayout.addTab(recallTab)
+        typeTabLayout.addTab(writingTab)
+
+        val tabStrip = typeTabLayout.getChildAt(0) as ViewGroup
+
+        for (i in 0 until tabStrip.childCount) {
+            val tabView = tabStrip.getChildAt(i)
+            if (tabView != null) {
+                val paddingStart = tabView.paddingStart
+                val paddingTop = tabView.paddingTop
+                val paddingEnd = tabView.paddingEnd
+                val paddingBottom = tabView.paddingBottom
+                ViewCompat.setBackground(
+                    tabView,
+                    AppCompatResources.getDrawable(tabView.context, R.drawable.intro_tab_selector)
+                )
+                tabView.minimumWidth = dpToPx(100)
+                ViewCompat.setPaddingRelative(
+                    tabView,
+                    paddingStart,
+                    paddingTop,
+                    paddingEnd,
+                    paddingBottom
+                )
+            }
+        }
+
+        typeTabLayout.setBackgroundColor(ContextCompat.getColor(this, android.R.color.transparent))
+        typeTabLayout.layoutParams =
+            LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT).apply {
+                setMargins(10)
+            }
+        typeTabLayout.tabGravity = GRAVITY_CENTER
+        typeTabLayout.tabMode = MODE_FIXED
+        typeTabLayout.setSelectedTabIndicator(null)
+        typeTabLayout.tabIconTint =
+            ColorStateList.valueOf(ContextCompat.getColor(this, R.color.white))
+
+        for (i in 0 until typeTabLayout.tabCount) {
+            val tab = (typeTabLayout.getChildAt(0) as ViewGroup).getChildAt(i)
+            val p = tab.layoutParams as ViewGroup.MarginLayoutParams
+            p.setMargins(15, 0, 15, 0)
+            tab.requestLayout()
+        }
+
+        typeTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> {
+                        if (isOnHiragana) {
+
+                            for (view in hiraganaWritingProgressViews) {
+                                progressLinearLayout.removeView(view)
+                            }
+
+                            for (view in hiraganaProgressViews) {
+                                progressLinearLayout.addView(view)
+                            }
+
+                            progressScrollView.fullScroll(ScrollView.FOCUS_UP)
+                        } else {
+
+                            for (view in katakanaWritingProgressViews) {
+                                progressLinearLayout.removeView(view)
+                            }
+
+                            for (view in katakanaProgressViews) {
+                                progressLinearLayout.addView(view)
+                            }
+                            progressScrollView.fullScroll(ScrollView.FOCUS_UP)
+                        }
+                    }
+                    1 -> {
+                        if (isOnHiragana) {
+
+                            for (view in hiraganaProgressViews) {
+                                progressLinearLayout.removeView(view)
+                            }
+
+                            for (view in hiraganaWritingProgressViews) {
+                                progressLinearLayout.addView(view)
+                            }
+                            progressScrollView.fullScroll(ScrollView.FOCUS_UP)
+                        } else {
+
+                            for (view in katakanaProgressViews) {
+                                progressLinearLayout.removeView(view)
+                            }
+
+                            for (view in katakanaWritingProgressViews) {
+                                progressLinearLayout.addView(view)
+                            }
+                            progressScrollView.fullScroll(ScrollView.FOCUS_UP)
+                        }
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+
+        progressLinearLayout.addView(typeTabLayout)
+    }
+
     /**
      * Adds the data for each kana into progress view array list
      * @param barList string array of labels attached to each bar
      * @param hiragana whether the bar is hiragana labels
      */
-    private fun loadBars(barList: List<Kana>) {
+    private fun loadBars(barList: List<Kana>, isWriting: Boolean) {
         for (kana in barList) {
 
             var color = 0
             var progress = 0f
 
-            if (kana.level != null) {
-                when (kana.level) {
+            val kanaLevel = if (isWriting) {
+                kana.writingLevel
+            } else {
+                kana.level
+            }
+
+            if (kanaLevel != null) {
+                when (kanaLevel) {
                     1 -> {
                         color = ContextCompat.getColor(this, R.color.rookie_pink)
                         progress = 1f
@@ -379,7 +573,6 @@ class StatsActivity : AppCompatActivity() {
             }
 
             val myProgressView = progressView(this) {
-
                 setProgress(progress)
                 setMin(0f)
                 setMax(6f)
@@ -403,7 +596,7 @@ class StatsActivity : AppCompatActivity() {
 
             myProgressView.setOnProgressClickListener {
                 if (kana.hasLearned) {
-                    KanaInfoView(this, kana, true).show()
+                    KanaInfoView(this, kana, true, writingInfo = false).show()
                 } else {
                     Toast.makeText(
                         this,
@@ -415,7 +608,7 @@ class StatsActivity : AppCompatActivity() {
 
             myProgressView.setOnClickListener {
                 if (kana.hasLearned) {
-                    KanaInfoView(this, kana, true).show()
+                    KanaInfoView(this, kana, true, writingInfo = false).show()
                 } else {
                     Toast.makeText(
                         this,
@@ -431,25 +624,16 @@ class StatsActivity : AppCompatActivity() {
             )
             params.setMargins(10, 15, 10, 15)
             myProgressView.layoutParams = params
-            if (kana.isHiragana) hiraganaProgressViews.add(myProgressView) else katakanaProgressViews.add(
-                myProgressView
-            )
+            if (isWriting) {
+                if (kana.isHiragana) hiraganaWritingProgressViews.add(myProgressView) else katakanaWritingProgressViews.add(
+                    myProgressView
+                )
+            } else {
+                if (kana.isHiragana) hiraganaProgressViews.add(myProgressView) else katakanaProgressViews.add(
+                    myProgressView
+                )
+            }
         }
-    }
-
-    private fun animateWave(view: View, startDelay: Long) {
-        val downAnimation = ObjectAnimator.ofFloat(view, "translationY", 30f)
-        val upAnimation = ObjectAnimator.ofFloat(view, "translationY", -30f)
-        val animationSet = AnimatorSet()
-        animationSet.duration = 1500
-        animationSet.startDelay = startDelay
-        animationSet.playSequentially(downAnimation, upAnimation)
-        animationSet.childAnimations.forEach {
-            val animation = it as ObjectAnimator
-            animation.repeatCount = Animation.INFINITE
-            animation.repeatMode = ValueAnimator.REVERSE
-        }
-        animationSet.start()
     }
 
     private fun animateInPlace(view: View) {
